66759a3ffae0fa0f73ecc94b98b718f2fa4f41aa
John Lima <ti.junior@gmail.com>
2022-08-31 16:26:12 -0300

    Implementação feed para buscar no firebase

---
+++ app/src/main/java/br/com/multalpha/aplicativos/v1/appinstagram/ui/profile/data/FireProfileDataSource.kt
@@ -0,0 +1,8 @@
+package br.com.multalpha.aplicativos.v1.appinstagram.ui.profile.data
+
+/**
+* Created by João Bosco on 29/08/2022.
+* e-mail - Support: ti.junior@gmail.com
+*/
+class FireProfileDataSource {
+}
